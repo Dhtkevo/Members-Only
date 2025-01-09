d143bde857dbd034665ffeed0f261b81f9cceceb
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-01-09 17:41:28 -0500

    Set up basic database queries and models

---
+++ controllers/populatedb.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+
+require("dotenv").config();
+const { Client } = require("pg");
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR ( 255 ) NOT NULL,
+  last_name VARCHAR ( 255 ),
+  username VARCHAR ( 255 ) NOT NULL UNIQUE,
+  password VARCHAR ( 255 ) NOT NULL,
+  membership_status BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR ( 255 ) NOT NULL,
+    time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    text VARCHAR ( 255 ) NOT NULL,
+    user_id INTEGER REFERENCES users (id) NOT NULL,
+);
+
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: process.env.CONNECTION_STRING,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
---
+++ db/queries.js
@@ -0,0 +1,24 @@
+const pool = require("./pool");
+
+async function getAllMessages() {
+  const { rows } = await pool.query("SELECT * FROM messages");
+  return rows;
+}
+
+async function createMessage(title, text, user_id) {
+  await pool.query(
+    "INSERT INTO messages (title, text, user_id) VALUES ($1, $2, $3)",
+    [title, text, user_id]
+  );
+}
+
+async function deleteMessage(id) {
+  await pool.query("DELETE FROM messages WHERE id = $1", [id]);
+}
+
+async function createUser(firstName, lastName, username, password, status) {
+  await pool.query(
+    "INSERT INTO users (first_name, last_name, username, password, membership_status) VALUES ($1, $2, $3, $4, $5)",
+    [firstName, lastName, username, password, status]
+  );
+}
