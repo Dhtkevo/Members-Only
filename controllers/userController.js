c30d84844c80fc1927271ccb90054b397a969de8
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-01-25 16:58:06 -0500

    Implement user validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,23 +3,54 @@ const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
+const validateUser = [
+  body("firstName")
+    .trim()
+    .notEmpty()
+    .isAlpha()
+    .withMessage("First name is invalid"),
+  body("lastName")
+    .trim()
+    .notEmpty()
+    .isAlpha()
+    .withMessage("Last name is invalid"),
+  body("username").trim().notEmpty().isEmail().withMessage("Email is invalid"),
+  body("password")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 4 })
+    .withMessage("Password is invalid"),
+  body("confirmPassword")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 4 })
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Passwords do not match!"),
+];
+
 exports.getRegisterUserForm = (req, res) => {
   res.render("sign-up");
 };
 
-exports.userCreatePost = asyncHandler(async (req, res, next) => {
-  const { firstName } = req.body;
-  const { lastName } = req.body;
-  const { username } = req.body;
-  const { password } = req.body;
-  const { confirmPassword } = req.body;
+exports.userCreatePost = [
+  validateUser,
+  asyncHandler(async (req, res, next) => {
+    const { firstName } = req.body;
+    const { lastName } = req.body;
+    const { username } = req.body;
+    const { password } = req.body;
 
-  if (password !== confirmPassword) {
-    return next(new Error("Passwords do not match!"));
-  }
+    const errors = validationResult(req);
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+    if (!errors.isEmpty()) {
+      return res.status(500).send(errors.array()[0].msg);
+    }
 
-  await db.createUser(firstName, lastName, username, hashedPassword);
-  return res.redirect("/");
-});
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await db.createUser(firstName, lastName, username, hashedPassword);
+    return res.redirect("/");
+  }),
+];
